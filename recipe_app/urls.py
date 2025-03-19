fad186eba0c56ee0e6344c2e211a650801d93962
Yash Shrivastava <115723600+yash-shriv@users.noreply.github.com>
2025-03-19 15:32:03 +0530

    Update urls.py
    Removed unnecessary comments.

--- recipe_app/urls.py
+++ recipe_app/urls.py
@@ -4,11 +4,9 @@ from .views import receipes, contact, about, success_page
 
 urlpatterns = [
     path('', views.receipes, name='receipes'),
-    # path('add_recipe/', views.add_recipe, name='add_recipe'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('success-page/', views.success_page, name='success_page'),
-    # path('recipes/delete/<int:id>/', views.delete_recipe, name='delete_recipe'),
     path('delete/<int:id>/', views.delete_recipe, name='delete_recipe'),
-    path('edit/<int:recipe_id>/', views.edit_recipe, name='edit_recipe'),  # New edit URL
+    path('edit/<int:recipe_id>/', views.edit_recipe, name='edit_recipe'),
 ]
